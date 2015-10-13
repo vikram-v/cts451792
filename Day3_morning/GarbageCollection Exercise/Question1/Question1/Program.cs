ca5bb8095528f8c1ebf78759077918b6a2d7682d
CTS451792 <SanjanaReddy.Avula@Cognizant.com>
2015-10-13 16:02:27 +0530

    GarbageCollection Exercise

---
+++ Day3_morning/GarbageCollection Exercise/Question1/Question1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+
+		/* Getting memory in bytes */
+
+		long initialBytes = GC.GetTotalMemory(false); 
+
+		/* After 10 million bytes are allocated */
+
+		byte[] memory = new byte[1000 * 1000 * 10];
+		memory[0] = 1;
+		long usedBytes = GC.GetTotalMemory(false);
+
+		/* After garbage collection */
+
+		long finalBytes = GC.GetTotalMemory(true);
+
+		Console.WriteLine(initialBytes);
+		Console.WriteLine(usedBytes);
+		Console.WriteLine(finalBytes - usedBytes);
+
+	}
+}
